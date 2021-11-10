ebd0d8e18f492d3d83a8c70cc1f24b6de6004bd2
NicolyAvelino <nicoly.oavelino@gmail.com>
2021-11-09 22:47:46 -0300

    Passo a Passo do Cadastro Comerciante

--- Cadastro/js/cadastro.js
+++ Cadastro/js/cadastro.js
@@ -1,47 +1,58 @@
-$(document).ready(function(){
+var currentTab = 0;
+showTab(currentTab);
 
-    /*  Activate the tooltips      */
-    $('[rel="tooltip"]').tooltip();
-
-
-        // Prepare the preview for profile picture
-        $("#picture").change(function(){
-            readURL(this);
-        });
-
-        $('[data-toggle="wizard-radio"]').click(function(){
-            wizard = $(this).closest('.wizard-card');
-            wizard.find('[data-toggle="wizard-radio"]').removeClass('active');
-            $(this).addClass('active');
-            $(wizard).find('[type="radio"]').removeAttr('checked');
-            $(this).find('[type="radio"]').attr('checked','true');
-        });
-
-        $('[data-toggle="wizard-checkbox"]').click(function(){
-            if( $(this).hasClass('active')){
-                $(this).removeClass('active');
-                $(this).find('[type="checkbox"]').removeAttr('checked');
-            } else {
-                $(this).addClass('active');
-                $(this).find('[type="checkbox"]').attr('checked','true');
-            }
-        });
-
-        $('.set-full-height').css('height', 'auto');
-
-    });
-
-
-
-//Function to show image before upload
-
-function readURL(input) {
-if (input.files && input.files[0]) {
-var reader = new FileReader();
-
-reader.onload = function (e) {
-    $('#PicturePreview').attr('src', e.target.result).fadeIn('slow');
+function showTab(n) {
+    var x = document.getElementsByClassName("tab");
+    x[n].style.display = "block";
+    if (n == 0) {
+        document.getElementById("prevBtn").style.display = "none";
+    } else {
+        document.getElementById("prevBtn").style.display = "inline";
+    }
+    if (n == (x.length - 1)) {
+        document.getElementById("nextBtn").innerHTML = "Voltar";
+    } else {
+        document.getElementById("nextBtn").innerHTML = "Avançar";
+    }
+    fixStepIndicator(n)
 }
-reader.readAsDataURL(input.files[0]);
+
+function nextPrev(n) {
+    var x = document.getElementsByClassName("tab");
+
+    if (n == 1 && !validateForm()) return false;
+    x[currentTab].style.display = "none";
+
+    currentTab = currentTab + n;
+
+    if (currentTab >= x.length) {
+        document.getElementById("regForm").submit();
+        return false;
+    }
+    showTab(currentTab);
 }
+
+function validateForm() {
+    var x, y, i, valid = true;
+    x = document.getElementsByClassName("tab");
+    y = x[currentTab].getElementsByTagName("input");
+
+    for (i = 0; i < y.length; i++) {
+        if (y[i].value == "") {
+            y[i].className += " invalid";
+            valid = false;
+        }
+    }
+    if (valid) {
+        document.getElementsByClassName("step")[currentTab].className += " finish";
+    }
+    return valid;
+}
+
+function fixStepIndicator(n) {
+    var i, x = document.getElementsByClassName("step");
+    for (i = 0; i < x.length; i++) {
+        x[i].className = x[i].className.replace(" active", "");
+    }
+    x[n].className += " active";
 }
