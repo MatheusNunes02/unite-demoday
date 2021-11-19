285c209b1ac00dcb26d362959405afb58fbf3a79
NicolyAvelino <nicoly.oavelino@gmail.com>
2021-11-19 19:59:14 -0300

    Arrumando Imagem
    Agora é possível ao clicar adicionar uma imagem

--- Cadastro/js/cadastro.js
+++ Cadastro/js/cadastro.js
@@ -56,3 +56,14 @@ function fixStepIndicator(n) {
     }
     x[n].className += " active";
 }
+// Adicionar Imagem
+function readImage() {
+    if (this.files && this.files[0]) {
+        var file = new FileReader();
+        file.onload = function(e) {
+            document.getElementById("preview").src = e.target.result;
+        };       
+        file.readAsDataURL(this.files[0]);
+    }
+}
+document.getElementById("img-input").addEventListener("change", readImage, false);
