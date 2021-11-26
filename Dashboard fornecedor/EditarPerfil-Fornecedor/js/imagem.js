3a197994b7b2d86437466ecfbee3dc56c5d0e8dd
NicolyAvelino <nicoly.oavelino@gmail.com>
2021-11-26 20:12:04 -0300

    Arrumando as imagens do Perfil Fornecedor

--- Dashboard fornecedor/EditarPerfil-Fornecedor/js/imagem.js
+++ Dashboard fornecedor/EditarPerfil-Fornecedor/js/imagem.js
@@ -9,3 +9,44 @@ function readImage() {
     }
 }
 document.getElementById("img-input").addEventListener("change", readImage, false);
+
+// Imagens Lado a Lado 
+
+// Imagem Um
+function rodarImagemUm() {
+    if (this.files && this.files[0]) {
+        var file = new FileReader();
+        file.onload = function(e) {
+            document.getElementById("ver").src = e.target.result;
+        };       
+        file.readAsDataURL(this.files[0]);
+    }
+}
+
+document.getElementById("imagem-input").addEventListener("change", rodarImagemUm, false);
+
+// Imagem Dois
+function rodarImagemDois() {
+    if (this.files && this.files[0]) {
+        var file = new FileReader();
+        file.onload = function(e) {
+            document.getElementById("verDois").src = e.target.result;
+        };       
+        file.readAsDataURL(this.files[0]);
+    }
+}
+
+document.getElementById("imagemDois-input").addEventListener("change", rodarImagemDois, false);
+
+// Imagem Tres
+function rodarImagemTres() {
+    if (this.files && this.files[0]) {
+        var file = new FileReader();
+        file.onload = function(e) {
+            document.getElementById("verTres").src = e.target.result;
+        };       
+        file.readAsDataURL(this.files[0]);
+    }
+}
+
+document.getElementById("imagemTres-input").addEventListener("change", rodarImagemTres, false);
