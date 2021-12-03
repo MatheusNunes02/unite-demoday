40646c8105f5ed8f42b2207f33cc2b2e0091347b
MatheusNunes02 <matheuscosta.nunes45@gmail.com>
2021-12-03 16:04:49 -0300

    atualizaaa

--- js/token.js
+++ js/token.js
@@ -7,10 +7,14 @@
   const api = axios.create({
     baseURL: "http://localhost:3000",
   });
+  console.log(parsed.token);
+  const result = await api.post("/checkAuthorization", {
+    email: parsed.email,
+    token: parsed.token,
+  });
 
-  const result = api.post(
-    "",
-    { email: parsed.email },
-    { headers: { token: parsed.token } }
-  );
+  console.log(result.data);
+  if (!result.data) {
+    return (window.location.href = "../../Login/login.html");
+  }
 })();
