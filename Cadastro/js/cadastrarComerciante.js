3fd5fe106340afae2bfe3fdce6958e20b25d47b6
MatheusNunes02 <matheuscosta.nunes45@gmail.com>
2021-12-06 10:52:56 -0300

    Cadastro de comerciantes, sem ter toda a validação

--- Cadastro/js/cadastrarComerciante.js
+++ Cadastro/js/cadastrarComerciante.js
@@ -1,17 +1,37 @@
 async function cadastrarComerciante() {
+  const formData = new FormData();
+
   const api = axios.create({
     baseURL: "http://localhost:3000",
   });
 
   const name = document.querySelector("#name").value;
-  const telefone = document.querySelector("telefone").value;
+  const telefone = document.querySelector("#telefone").value;
   const cpf = document.querySelector("#cpf").value;
   const email = document.querySelector("#email").value;
   const password = document.querySelector("#password").value;
   const cep = document.querySelector("#cep").value;
-  const bairro = document.querySelector("bairro").value;
-  const endereco = document.querySelector("endereco").value;
-  const img = document.querySelector("#img-input").value;
+  const bairro = document.querySelector("#bairro").value;
+  const endereco = document.querySelector("#endereco").value;
+  const img = document.querySelector("#img-input");
+  const sobre_mim = document.querySelector("#FormControlTextarea").value;
 
-  console.log(result.data);
+  formData.append("name", name);
+  formData.append("telefone", telefone);
+  formData.append("cpf", cpf);
+  formData.append("email", email);
+  formData.append("password", password);
+  formData.append("cep", cep);
+  formData.append("bairro", bairro);
+  formData.append("endereco", endereco);
+  formData.append("sobre_mim", sobre_mim);
+  formData.append("file", img.files[0]);
+
+  const result = await api
+    .post("/merchants", formData, {
+      headers: {
+        "Content-Type": `multipart/form-data; boundary=${formData._boundary}`,
+      },
+    })
+    .then((response) => console.log(response));
 }
--- Cadastro/js/cadastro.js
+++ Cadastro/js/cadastro.js
@@ -48,6 +48,101 @@ function validateForm() {
       y[i].classList.remove("invalid");
     }
   }
+
+  if (currentTab == 0) {
+    let name = document.querySelector("#name").value;
+    let telefone = document.querySelector("#telefone").value;
+    let cpf = document.querySelector("#cpf").value;
+    let email = document.querySelector("#email").value;
+    let password = document.querySelector("#password").value;
+    if (!name) {
+      document
+        .querySelector("#name")
+        .setAttribute("placeholder", "Insira um nome válido");
+
+      document.querySelector("#name").classList.add("invalid");
+      valid = false;
+    }
+
+    if (telefone.length != 11) {
+      document
+        .querySelector("#telefone")
+        .setAttribute("placeholder", "Telefone inválido, Ex:11999990000");
+      document.querySelector("#telefone").classList.add("invalid");
+      document.querySelector("#telefone").value = "";
+      valid = false;
+    }
+
+    if (cpf.length != 11) {
+      document
+        .querySelector("#cpf")
+        .setAttribute("placeholder", "Cpf inválido, Ex:99933355500");
+      document.querySelector("#cpf").classList.add("invalid");
+      document.querySelector("#cpf").value = "";
+      valid = false;
+    }
+
+    if (!email || email.indexOf("@") == -1 || email.indexOf(".com") == -1) {
+      document
+        .querySelector("#email")
+        .setAttribute("placeholder", "Email inválido");
+      document.querySelector("#email").classList.add("invalid");
+      document.querySelector("#email").value = "";
+      valid = false;
+    }
+
+    if (!password) {
+      document
+        .querySelector("#password")
+        .setAttribute("placeholder", "Senha vazia");
+      document.querySelector("#password").classList.add("invalid");
+      valid = false;
+    }
+  } else if (currentTab == 1) {
+    let cep = document.querySelector("#cep").value;
+    let bairro = document.querySelector("#bairro").value;
+    let endereco = document.querySelector("#endereco").value;
+
+    if (!cep) {
+      document
+        .querySelector("#cep")
+        .setAttribute("placeholder", "Cep inválido");
+      document.querySelector("#cep").classList.add("invalid");
+      document.querySelector("#cep").value = "";
+      valid = false;
+    }
+
+    if (!bairro) {
+      document
+        .querySelector("#bairro")
+        .setAttribute("placeholder", "Bairro inválido");
+      document.querySelector("#bairro").classList.add("invalid");
+      document.querySelector("#bairro").value = "";
+      valid = false;
+    }
+
+    if (!endereco) {
+      document
+        .querySelector("#endereco")
+        .setAttribute("placeholder", "Endereço inválido");
+      document.querySelector("#endereco").classList.add("invalid");
+      document.querySelector("#endereco").value = "";
+      valid = false;
+    }
+  } else {
+    let img = document.querySelector("#img-input").value;
+    let sobrenos = document.querySelector("#FormControlTextarea").value;
+
+    if (!sobrenos) {
+      document
+        .querySelector("#FormControlTextarea")
+        .setAttribute("placeholder", "Insira alguma informação");
+      document.querySelector("#FormControlTextarea").classList.add("invalid");
+      document.querySelector("#FormControlTextarea").value = "";
+      valid = false;
+    }
+  }
+
   if (valid) {
     document.getElementsByClassName("step")[currentTab].className += " finish";
   } else {
