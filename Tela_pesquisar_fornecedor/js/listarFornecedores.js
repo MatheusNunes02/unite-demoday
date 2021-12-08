f4772b19103b9127eab552afdaaa18197e3de9e1
MatheusNunes02 <88735983+MatheusNunes02@users.noreply.github.com>
2021-12-07 22:14:32 -0300

    Listagem

---
+++ Tela_pesquisar_fornecedor/js/listarFornecedores.js
@@ -0,0 +1,45 @@
+(async function validate() {
+    const api = axios.create({
+      baseURL: "http://localhost:3000",
+    });
+    const content = document.getElementById("containerCardFornecedorId")
+    const result = await api.get("/suppliers");
+
+    if(!result.data){
+        content.innerHTML = `Erro no servidor :/`
+    }else{
+        result.data.forEach(element => {
+            content.innerHTML += `
+            <div class="cardExemplo"> 
+            <div class="divFotoperfil">
+              <img src="http://localhost:3000/uploads/${element.foto}">
+            </div>
+            <div class="containerNomeCateoria">
+              <h3>${element.nome_empresa}</h3>
+              <div>
+                <p>
+                  ${element.descricao}
+                </p>
+              </div>
+            </div>
+            <div class="containerBotaoStar">
+              <a href="../Dashboard_comerciante/chats/chat-Comerciante.html"><button>Entre em contato</button></a>
+              <div>
+              <span class="fa fa-star checked"></span>
+              <span class="fa fa-star checked"></span>
+              <span class="fa fa-star checked"></span>
+              <span class="fa fa-star checked"></span>
+              <span class="fa fa-star"></span>
+              </div>
+            </div>
+          </div>
+    
+            `;
+        });
+    }
+
+     
+
+    content.innerHTML
+    console.log(result);
+})();
